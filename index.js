7439c49a1240583805059be4b2b8b596e21a77ce
mgordonabc <mgordonabc@gmail.com>
2015-08-28 01:01:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,20 +2,24 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var user_count = 0;
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+  
+  //increase the number of users and send the count 
+  user_count = user_count + 1;
+  io.emit('dashboard message', user_count);
+  
+  //send chat msg 
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
     console.log('message: ' + msg);
   });
-  
-  socket.on('dashboard message', function(msg){
-    io.emit('dashboard message', msg);
-    console.log('dashboard message: ' + msg);
-  });
+
 });
 
 http.listen(3000, function(){
