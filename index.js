1cfcfac888faf723405338e92524a63bbcf98f43
mgordonabc <mgordonabc@gmail.com>
2015-08-28 00:32:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ io.on('connection', function(socket){
   });
 });
 
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});
+
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
+
+
