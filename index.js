1a4ad5aa990472d144e3475779a78e119661aadf
mgordonabc <mgordonabc@gmail.com>
2015-08-28 00:33:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
   });
