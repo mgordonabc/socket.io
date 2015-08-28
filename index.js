294bb37549cd70c084cf02d95a68ced999a927b5
mgordonabc <mgordonabc@gmail.com>
2015-08-28 00:46:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,11 +8,10 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
     console.log('message: ' + msg);
   });
 });
-
-io.emit('some event', { for: 'everyone' });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
