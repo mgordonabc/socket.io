210c517959a6f42fcf689a200027a7e7c0fa64da
mgordonabc <mgordonabc@gmail.com>
2015-08-29 16:12:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-
-
+user_names = "";
 user_count = 0;
 
 app.get('/', function(req, res){
